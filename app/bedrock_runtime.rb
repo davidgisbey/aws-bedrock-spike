7ad5457fc67993ecc4de3ad01c4182727627ddb7
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-02-26 12:38:12 +0000

    Add endpoint for getting embedding for query

--- app/bedrock_runtime.rb
+++ app/bedrock_runtime.rb
@@ -2,14 +2,14 @@ require 'sinatra'
 require 'dotenv/load'
 require 'aws-sdk-bedrockruntime'
 
+client = Aws::BedrockRuntime::Client.new(
+  region: ENV['REGION'],
+  access_key_id: ENV['ACCESS_KEY_ID'],
+  secret_access_key: ENV['SECRET_ACCESS_KEY']
+)
+
 get '/query' do
   query = params[:query]
-
-  client = Aws::BedrockRuntime::Client.new(
-    region: ENV['REGION'],
-    access_key_id: ENV['ACCESS_KEY_ID'],
-    secret_access_key: ENV['SECRET_ACCESS_KEY']
-  )
 
   aws_response = client.invoke_model(
     model_id: 'amazon.titan-text-express-v1',
@@ -28,4 +28,18 @@ get '/query' do
   )
 
   JSON.parse(aws_response.body.read)["results"].first["outputText"]
+end
+
+get '/get-embedding' do
+  query = params[:query]
+
+  aws_response = client.invoke_model(
+  model_id: "amazon.titan-embed-text-v1",
+  content_type: 'application/json',
+  accept: '*/*',
+  body: {
+    inputText: query,
+  }.to_json
+)
+  JSON.parse(aws_response.body.read)["embedding"].to_s
 end
