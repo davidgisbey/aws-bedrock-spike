4ae23f4950b0ad12cd805fb343986443b35e73ab
davidgisbey <david.gisbey@digital.cabinet-office.gov.uk>
2024-02-26 11:21:33 +0000

    Add endpoint which uses aws-sdk-bedrockruntime instead of ruby ruby-amazon-bedrock gem
    This shows a very basic implementation for calling the AWS SDK for Ruby V3
    to call the BedrockRuntime API instead of using the ruby-amazon-bedrock gem.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@
 
 source "https://rubygems.org"
 
+gem "aws-sdk-bedrockruntime"
 gem "ruby-amazon-bedrock"
 gem 'sinatra'
 
--- app.rb
+++ app.rb
@@ -2,12 +2,41 @@ require 'sinatra'
 require 'pry'
 require 'dotenv/load'
 require "amazon_bedrock"
+require 'aws-sdk-bedrockruntime'
 
 get '/ping' do
   'pong'
 end
 
 get '/query' do
+  query = params[:query]
+
+  client = Aws::BedrockRuntime::Client.new(
+    region: ENV['REGION'],
+    access_key_id: ENV['ACCESS_KEY_ID'],
+    secret_access_key: ENV['SECRET_ACCESS_KEY']
+  )
+
+  aws_response = client.invoke_model(
+    model_id: 'amazon.titan-text-express-v1',
+    content_type: 'application/json',
+    accept: '*/*',
+    body: {
+      inputText: query,
+      # you would want to make these configurable, but for simplicity hardcoded them here.
+      textGenerationConfig: {
+        maxTokenCount: 4096,
+        stopSequences: [],
+        temperature: 0,
+        topP: 1
+      }
+    }.to_json
+  )
+
+  JSON.parse(aws_response.body.read)["results"].first["outputText"]
+end
+
+get '/query-with-ruby-amazon-bedrock' do
   query = params[:query]
 
   client = RubyAmazonBedrock::Client.new(
